f41a292bd3d53b31e47661201480537764924a2f
Koki Akamatsu <list6311@gmail.com>
2020-12-17 20:36:57 +0900

    study cs

---
+++ DestroyParticlesystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticlesystem : MonoBehaviour
+{
+    //オブジェクトの消滅時間
+    public float destroyTime = 5f;
+
+    void Start()
+    {
+        //コールチンdestroyParticlesytemのコール
+        StartCoroutine(destroyParticlesytem());
+    }
+
+    //オブジェクトを指定された時間で消滅させる
+    IEnumerator destroyParticlesytem()
+    {
+        yield return new WaitForSeconds(destroyTime);
+        Destroy(this.gameObject);
+    }
+}
---
+++ SwordController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordController : MonoBehaviour{
+    public GameObject ParticlePrefabs;
+    public GameObject cubePrefabs;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "BOX")
+        {
+            GameObject particle = Instantiate(ParticlePrefabs) as GameObject;
+            Vector3 hitPos = this.transform.position;
+            Debug.Log("hitPos="  + hitPos);
+            particle.transform.position = hitPos;
+            Destroy(cubePrefabs);
+        }
+    }
+}
